30f2f4efdc4aebc17d00070dc7ecd875e9fe2230
huangjihua <steven.online@qq.com>
2023-08-13 18:23:17 +0800

    fix: 优化 react-icons打包

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'father';
 
 export default defineConfig({
-  plugins: ['father-plugin-dumi-theme'],
-  cjs: {}
+  esm: {
+    output: 'dist/esm'
+  },
+  cjs: {
+    output: 'dist/cjs'
+  },
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'react-icons',
+        libraryDirectory: 'lib/icons',
+        camel2DashComponentName: false
+      },
+      'react-icons'
+    ]
+  ],
+  plugins: ['father-plugin-dumi-theme']
 });
